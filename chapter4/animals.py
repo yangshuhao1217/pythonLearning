dbbb9f680ed6462c2eb0f1389a4a2efea1357140
yangshuhao1217 <yangshuhao017@gmail.com>
2020-12-30 21:58:23 +1000

    Tenth commit

---
+++ chapter4/animals.py
@@ -0,0 +1,4 @@
+animals = ['dog', 'cat', 'bird', 'horse']
+for animal in animals:
+    print(f"A {animal} would make a good friend.")
+print("Any of these animals would make a great friend.")
---
+++ chapter4/pizzas.py
@@ -0,0 +1,4 @@
+pizzas = ['peperoni', 'margorita', 'chesari', 'nikocia']
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+print("I really love pizza!")
