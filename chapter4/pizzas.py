2fe521dbb822229b48f47932bf22f6c7352c7e2e
yangshuhao1217 <yangshuhao017@gmail.com>
2021-01-01 15:54:18 +1000

    Twelveth commit

--- chapter4/pizzas.py
+++ chapter4/pizzas.py
@@ -1,4 +1,10 @@
 pizzas = ['peperoni', 'margorita', 'chesari', 'nikocia']
+friend_pizzas = pizzas[:]
+pizzas.append('sicili')
+friend_pizzas.append('lanita')
+print("My favorite pizzas are:")
 for pizza in pizzas:
-    print(f"I like {pizza} pizza.")
-print("I really love pizza!")
+    print(pizza)
+print("My friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
---
+++ chapter4/players.py
@@ -0,0 +1,4 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+print(players[1:4])
+print(players[2:])
