b6b07ee2f70548c922879e106c8812554c43c7aa
yangshuhao1217 <yangshuhao017@gmail.com>
2021-01-01 16:13:01 +1000

    Thirteenth commit

---
+++ buffet.py
@@ -0,0 +1,7 @@
+foods = ('carrot cake', 'hot dog', 'lassana', 'cheese cake', 'shwarma')
+for food in foods:
+    print(food)
+
+foods = ('carrot cake', 'blueberry pie', 'hot dog', 'sandwich', 'shawarma')
+for food in foods:
+    print(food)
