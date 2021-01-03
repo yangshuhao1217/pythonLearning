7f7695739119b742747c50c4d48e6187e16f7012
yangshuhao1217 <yangshuhao017@gmail.com>
2021-01-03 23:03:12 +1000

    Fourteenth commit

---
+++ chapter5/alien_colors.py
@@ -0,0 +1,5 @@
+alien_color = 'green'
+
+if alien_color == 'red':
+    print("You just earned 5 points.")
+
---
+++ chapter5/favorite_fruits.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['apple', 'banana', 'watermelon']
+
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
+if 'tangerine' in favorite_fruits:
+    print("You really like tangerines!")
+if 'banana' in favorite_fruits:
+    print("You really like bananas!")
+if 'watermelon' in favorite_fruits:
+    print("You really like watermelons.")
+if 'orange' in favorite_fruits:
+    print("You really like oranges.")
