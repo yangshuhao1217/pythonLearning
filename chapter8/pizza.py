ce97e1ba58a50300c62d9b695fe630879d6d3905
yangshuhao1217 <yangshuhao017@gmail.com>
2021-01-27 22:25:56 +1000

    30th commit

--- chapter8/pizza.py
+++ chapter8/pizza.py
@@ -4,5 +4,3 @@ def make_pizza(size, *toppings):
     for topping in toppings:
         print(f"- {topping}")
 
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
