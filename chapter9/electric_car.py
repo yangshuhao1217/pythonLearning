def8473792300097c0422ee1a2b848db5c0228de
yangshuhao1217 <yangshuhao017@gmail.com>
2021-03-04 21:44:06 +1000

    37th commit

--- chapter9/electric_car.py
+++ chapter9/electric_car.py
@@ -1,26 +1,6 @@
-class Car:
-    """A simple attempt to represent a car."""
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
+"""A set of classes that can be used to represent electric cars."""
 
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
-
-    def read_odometer(self):
-        print(f"This car has {self.odometer_reading} miles on it.")
-
-    def update_odometer(self, mileage):
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
-
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
+from car import Car
 
 class Battery:
     """A simple attempt to model a battery foe an electric car."""
@@ -61,15 +41,3 @@ class ElectricCar(Car):
         self.battery = Battery()
 
 
-print("Make an electric car, and check the battery:")
-my_tesla = ElectricCar('tesla', 'model s', 2019)
-my_tesla.battery.describe_battery()
-
-
-print("\nUpgrade the battery, and check it again:")
-my_tesla.battery.upgrade_battery()
-my_tesla.battery.describe_battery()
-
-print("\nTry upgrading the battery a second time.")
-my_tesla.battery.upgrade_battery()
-my_tesla.battery.describe_battery()
--- chapter9/my_car.py
+++ chapter9/my_car.py
@@ -1,7 +1,8 @@
-from car import Car, ElectricCar
+from car import Car
+from electric_car import ElectricCar as EC
 
 my_beetle = Car('volswagen', 'beetle', 2019)
 print(my_beetle.get_descriptive_name())
 
-my_tela = ElectricCar('tesla', 'roadster', 2019)
+my_tela = EC('tesla', 'roadster', 2019)
 print(my_tela.get_descriptive_name())
