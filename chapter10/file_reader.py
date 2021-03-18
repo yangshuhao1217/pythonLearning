79168f56236cba3b79a3c5f8729f1c0c19a9bcac
yangshuhao1217 <yangshuhao017@gmail.com>
2021-03-18 22:35:32 +1000

    41 commit

--- chapter10/file_reader.py
+++ chapter10/file_reader.py
@@ -1,3 +1,5 @@
-with open('pi_digits.txt') as file_object:
-    contents = file_object.read()
-print(contents.rstrip())
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
