2937e941e75e4390cef1be12c2a56e83801641d0
yangshuhao1217 <yangshuhao017@gmail.com>
2021-04-13 22:39:37 +1000

    49 commit

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """Overall class to manage game assert and behavior."""
@@ -8,18 +9,22 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        
+
+                
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse envents.
+            # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
