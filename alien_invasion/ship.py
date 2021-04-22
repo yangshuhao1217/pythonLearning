3a9e872171a2d834cebd7f402b30674699c3a90e
yangshuhao1217 <yangshuhao017@gmail.com>
2021-04-22 22:44:18 +1000

    move up and down rocket

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -16,13 +16,18 @@ class Ship:
 
 		# Start each new ship at the bottom center of the screen.
 		self.rect.midbottom = self.screen_rect.midbottom
+		self.rect.center = self.screen_rect.center
+		
 
 		# Store a decimal value for the ship's horizontal positon.
 		self.x = float(self.rect.x)
+		self.y = float(self.rect.y)
 
 		# Movement flags
 		self.moving_right = False
 		self.moving_left = False
+		self.moving_up = False
+		self.moving_down = False
 
 	def update(self):
 		"""Update the ship's position based on the movement flag."""
@@ -31,10 +36,15 @@ class Ship:
 			self.x += self.settings.ship_speed
 		if self.moving_left and self.rect.left > 0:
 			self.x -= self.settings.ship_speed
+		if self.moving_up and self.rect.top > 0:
+			self.y -= self.settings.ship_speed
+		if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+			self.y += self.settings.ship_speed
 
-		# Update rect object from self.x
+		# Update rect object from self.x and self.y
 		self.rect.x = self.x
-
+		self.rect.y = self.y
+		
 	def blitme(self):
 		"""Draw the ship at its current location."""
 		self.screen.blit(self.image, self.rect)
