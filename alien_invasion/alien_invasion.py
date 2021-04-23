fc424e3e7d4e04ae24878903d9640039bd7f4b0b
yangshuhao1217 <yangshuhao017@gmail.com>
2021-04-23 22:54:04 +1000

    53 commit

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -28,7 +28,7 @@ class AlienInvasion:
         	self._update_screen()
 
     def _check_events(self):
-    	"""Respond to keypresses and mouse events."""
+    	"""Responds to keypresses and mouse events."""
     	for event in pygame.event.get():
     		if event.type == pygame.QUIT:
     			sys.exit()
@@ -38,11 +38,15 @@ class AlienInvasion:
     			self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
-        """Respond to keypresses."""
+        """Responds to keypresses."""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+        	self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+        	self.ship.moving_down = True
         elif event.key == pygame.K_q:
             sys.exit()
     
@@ -52,6 +56,10 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left =False
+        elif event.key == pygame.K_UP:
+        	self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+        	self.ship.moving_down = False
         
     def _update_screen(self):
     	"""Update images on the screen, and flip to the new screen."""
