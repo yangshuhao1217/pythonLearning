e6ade6465579f6e6795c042d7ddc8e60d5faa9d2
yangshuhao1217 <yangshuhao017@gmail.com>
2021-04-27 22:23:53 +1000

    54 commit

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -27,14 +27,7 @@ class AlienInvasion:
         while True:
         	self._check_events()
         	self.ship.update()
-        	self.bullets.update()
-
-        	#Get rid of bullets taht have disappeared.
-        	for bullet in self.bullets.copy():
-        		if bullet.rect.bottom <= 0:
-        			self.bullets.remove(bullet)
-        	
-
+        	self._update_bullets()
         	self._update_screen()
 
     def _check_events(self):
@@ -75,8 +68,18 @@ class AlienInvasion:
 
     def _fire_bullet(self):
     	"""Create a new bullet and add it to the bullets froup."""
-    	new_bullet = Bullet(self)
-    	self.bullets.add(new_bullet)
+    	if len(self.bullets) < self.settings.bullets_allowed:
+    		new_bullet = Bullet(self)
+    		self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+    	"""Update positon of bullets and get rid of old bullets."""
+    	# Update bullet  positions.
+    	self.bullets.update()
+    	# Get rid of bullets that have disappesred.
+    	for bullet in self.bullets.copy():
+    		if bullet.rect.bottom <= 0:
+    			self.bullets.remove(bullet)
         
     def _update_screen(self):
     	"""Update images on the screen, and flip to the new screen."""
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -31,7 +31,7 @@ class Ship:
 
 	def update(self):
 		"""Update the ship's position based on the movement flag."""
-		# Update the ship's x value, not the rec.
+		# Update the ship's x, y value, not the rec.
 		if self.moving_right and self.rect.right < self.screen_rect.right:
 			self.x += self.settings.ship_speed
 		if self.moving_left and self.rect.left > 0:
