13a62688022c1023d96782ebd8947e5794004b0f
yangshuhao1217 <yangshuhao017@gmail.com>
2021-05-18 23:24:43 +1000

    59 commit

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -36,9 +36,12 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+            
             self._update_screen()
 
     def _check_events(self):
@@ -117,22 +120,37 @@ class AlienInvasion:
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
 
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
+                
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
 
-        # Decrement ships_left.
-        self.stats.ships_left -= 1
+        if self.stats.ships_left >= 0:
+            self.stats.ships_left -= 1
 
-        # Get rid of any remaining aliens and bullets.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Create a new fleet and center the ship
-        self._create_fleet()
-        self.ship.center_ship()
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Pause.
-        sleep(0.5) 
+            # Pause.
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
+
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -161,6 +179,7 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
 
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reached an edge."""
