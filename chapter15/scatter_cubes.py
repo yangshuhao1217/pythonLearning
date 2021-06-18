5a004347390e9783c94ec156710d952fc4ec5b75
yangshuhao1217 <yangshuhao017@gmail.com>
2021-06-18 15:29:54 +1000

    68 commit

--- chapter15/scatter_cubes.py
+++ chapter15/scatter_cubes.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-x_values = range(1, 1001)
-y_values = [x**2 for x in x_values]
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
 
 plt.style.use('seaborn')
 
@@ -9,13 +9,13 @@ fig, ax = plt.subplots()
 ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
-ax.set_title("Square Numbers", fontsize=24)
+ax.set_title("Cube Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Square of Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
 
-# Set size of tick labels.
+# Set size of tick labes.
 ax.tick_params(axis='both', which='major', labelsize=14)
 
 # Set the range for each axis.
-ax.axis([0, 1100, 0, 1100000])
-plt.savefig('squares_plot.png', bbox_inches='tight')
+ax.axis([0, 5100, 0, 130000000000])
+plt.show()
