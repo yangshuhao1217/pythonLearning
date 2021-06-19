2956b98bd110dce04565ec4d3ad01931ca1dcd3b
yangshuhao1217 <yangshuhao017@gmail.com>
2021-06-19 14:49:50 +1000

    69 commit

--- chapter15/rw_visual.py
+++ chapter15/rw_visual.py
@@ -2,13 +2,26 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk
+# Keep making new walks, as long as the program is active.
+while True:
+    # Make a random walk
 
-rw = RandomWalk()
-rw.fill_walk()
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
 
-# Plot the points in the walk.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+        edgecolors='none', s=1)
+
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
