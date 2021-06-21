cf0b2f04d6e70b4058ffbdb5811aa34cafefc374
yangshuhao1217 <yangshuhao017@gmail.com>
2021-06-21 16:23:22 +1000

    70 commit

--- chapter15/random_walk.py
+++ chapter15/random_walk.py
@@ -10,6 +10,14 @@ class RandomWalk:
         # All walks start at(0, 0).
         self.x_values = [0]
         self.y_values = [0]
+    
+
+    def get_step(self):
+        """Decide the direction and distance of the walk."""
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4])
+        step = direction * distance
+        return step
 
     def fill_walk(self):
         """Calculate all the points in the walk."""
@@ -18,13 +26,8 @@ class RandomWalk:
         while len(self.x_values) < self.num_points:
 
             # Decide which direction to go and how far to go in that direction.
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # Reject moves that go nowhere.
             if x_step == 0 and y_step  == 0:
--- chapter15/rw_visual.py
+++ chapter15/rw_visual.py
@@ -6,15 +6,18 @@ from random_walk import RandomWalk
 while True:
     # Make a random walk
 
-    rw = RandomWalk(50_000)
+    rw = RandomWalk(5_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
+    fig, ax = plt.subplots(figsize=(16, 9))
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-        edgecolors='none', s=1)
+    ax.plot(rw.x_values, rw.y_values, linewidth=1)
+
+    # First and last dot.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
     # Remove the axes.
     ax.get_xaxis().set_visible(False)
