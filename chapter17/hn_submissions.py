b8a9c0f23177a03921e6f5f0ea144279943cf31d
yangshuhao1217 <yangshuhao017@gmail.com>
2021-07-19 17:55:54 +1000

    82 commit

--- chapter17/hn_submissions.py
+++ chapter17/hn_submissions.py
@@ -1,6 +1,8 @@
 from operator import itemgetter
 
 import requests
+from plotly.graph_objs import Bar
+from plotly import offline
 
 # Make an API call and store the response.
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
@@ -32,3 +34,46 @@ for submission_dict in submission_dicts:
     print(f"\nTitle: {submission_dict['title']}")
     print(f"Discussion link: {submission_dict['hn_link']}")
     print(f"Comments: {submission_dict['comments']}")
+
+# Generate lists for plotting.
+titles, num_comments, discn_links = [], [], []
+for sd in submission_dicts:
+    title = sd['title']
+    hn_link = sd['hn_link']
+    discn_link = f"<a href='{hn_link}'>{title[:15]}...</a>"
+
+    titles.append(title)
+    num_comments.append(sd['comments'])
+    discn_links.append(discn_link)
+
+# Make visualization.
+data = [{
+    'type': 'bar',
+    'x': discn_links,
+    'y': num_comments,
+    'hovertext': titles,
+    'marker':{
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'},
+    },
+    'opacity': 0.6,
+}]
+
+
+my_layout = {
+    'title': 'Most_discussed articles on Hacker News',
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'Article',
+        'titlefont': {'size': 24},
+        'tickfont': {'size': 14},
+    },
+    'yaxis':{
+        'title': 'Number of Comments',
+        'titlefont': {'size': 24},
+    'tickfont': {'size': 14},
+    },
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='hn_discussions.html')
---
+++ chapter17/perl_repos.py
@@ -0,0 +1,36 @@
+import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
+
+# Make an API call and store the response
+url = 'https://api.github.com/search/repositories?q=language:perl&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+# Process results.
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_links, stars, labels = [], [], []
+for repo_dict in repo_dicts:
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+
+    stars.append(repo_dict['stargazers_count'])
+
+# Make visualization.
+data = [{
+    'type': 'bar',
+    'x': repo_links,
+    'y': stars,
+}]
+my_layout = {
+    'title': 'Most_Starred Perl Projects on Github',
+    'xaxis': {'title': 'Repository'},
+    'yaxis': {'title': 'Stares'},
+}
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='perl_repos.html')
