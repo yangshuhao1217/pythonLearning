b4d05f7fff3e91d0daa55bb9a3a2d4c04d74be21
yangshuhao1217 <yangshuhao017@gmail.com>
2021-07-26 22:08:27 +1000

    86 commit

--- pizzeria/pizzas/models.py
+++ pizzeria/pizzas/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 class Pizza(models.Model):
-    """Pizza info."""
+    """Pizza name."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Return a string representation of the model."""
         return self.text
+class Topping(models.Model):
+    """Topping info of every pizza."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'toppings'
+
+    def __str__(self):
+        return f"{self.text}"
