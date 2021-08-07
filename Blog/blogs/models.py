f527e5e19c5a9445815b9d0bd094608922bc6ee9
yangshuhao1217 <yangshuhao017@gmail.com>
2021-08-07 15:30:45 +1000

    95 commit

---
+++ Blog/blogs/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class BlogPost(models.Model):
+    """A BlogPost the user is posting on Blog."""
+    title = models.CharField(max_length=100)
+    date_added = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        """Return a string representaation of the model."""
+        return self.title
+
+class Entry(models.Model):
+    """Post"""
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    text = models.TextField()
+    dated_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.text[:50]}..."
---
+++ Blog/blogs/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def index(request):
+    """The home page for Blog."""
+    return render(request, 'blogs/index.html')
