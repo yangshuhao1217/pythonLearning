0c1857c2bded9511aafe9eb7c9f84601e02c1416
yangshuhao1217 <yangshuhao017@gmail.com>
2021-09-10 17:40:15 +1000

    101 commit

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'blogs',
     'users',
 
+    # Third party apps.
+    'bootstrap4',
+
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
@@ -128,3 +131,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings
+LOGIN_URL = 'users:login'
--- Blog/blogs/views.py
+++ Blog/blogs/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
 from .models import BlogPost, Entry
 from .forms import EntryForm, PostForm
 
@@ -6,19 +9,26 @@ def index(request):
     """The home page for Blog."""
     return render(request, 'blogs/index.html')
 
+@login_required
 def posts(request):
     """Show all posts."""
-    posts = BlogPost.objects.order_by('date_added')
+    posts = BlogPost.objects.filter(owner=request.user).order_by('date_added')
     context = {'posts': posts}
     return render(request, 'blogs/posts.html', context)
 
+@login_required
 def post(request, post_id):
     """Show a single post and all its entries."""
     post = BlogPost.objects.get(id=post_id)
+    # Make sure the psot belongs to the current user.
+    if post.owner != request.user:
+        raise Http404
+
     entries = post.entry_set.order_by('date_added')
     context = {'post': post, 'entries': entries}
     return render(request, 'blogs/post.html', context)
 
+@login_required
 def new_post(request):
     """Add a new post."""
     if request.method != 'POST':
@@ -28,16 +38,22 @@ def new_post(request):
         # POST data submitted; process data.
         form = PostForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_post = form.save(commit=False)
+            new_post.owner = request.user
+            new_post.save()
             return redirect('blogs:posts')
     
     # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'blogs/new_post.html', context)
 
+@login_required
 def new_entry(request, post_id):
     """Add a new entry for a particular post."""
     post = BlogPost.objects.get(id=post_id)
+    if post.owner != request.user:
+        raise Http404
+
     if request.method != 'POST':
         # No data submitted; create a blank form.
         form = EntryForm()
@@ -53,10 +69,13 @@ def new_entry(request, post_id):
     context = {'post': post, 'form': form}
     return render(request, 'blogs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     post = entry.post
+    if post.owner != request.user:
+        raise Http404
 
     if request.method !='POST':
         # Initial request; pre-fill form with the current entry.
