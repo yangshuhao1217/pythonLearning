5e8c8378b4f43cc57d91a8832e8e6ff0ee38473b
Yang Shuhao <yangshuhao017@gmail.com>
2021-12-16 13:49:38 +1000

    update my_own

--- my_own/pdf_split.py
+++ my_own/pdf_split.py
@@ -1,9 +1,16 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
+from os import listdir
+import PySimpleGUI as sg
 
-inputpdf = PdfFileReader(open("document.pdf", "rb"))
+inputfile = sg.popup_get_file("Please choose file")
+
+
+inputpdf = PdfFileReader(open(inputfile, "rb"))
+
+outputdir = sg.popup_get_folder('Where to save') + '/'
 
 for i in range(inputpdf.numPages):
     output = PdfFileWriter()
     output.addPage(inputpdf.getPage(i))
-    with open("document-page%s.pdf" % i, "wb") as outputStream:
+    with open(f'{outputdir}document-page%s.pdf' % i, "wb") as outputStream:
         output.write(outputStream)
