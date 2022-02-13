cd4d972c617898275f00f76b97ef1b7114922b12
yangshuhao1217 <yangshuhao017@gmail.com>
2022-02-13 18:41:41 +1000

    boring stuff chapter02

---
+++ boringStuff/chapter02/vampire2.py
@@ -0,0 +1,10 @@
+name = 'Carol'
+age = 3000
+if name == 'Alice':
+    print('hi, Alice')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+elif age > 100:
+    print('You are not Alice, grannie.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
