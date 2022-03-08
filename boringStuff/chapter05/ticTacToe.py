5d9ee2c1d23105526fe891403e383876857af550
Yang Shuhao <yangshuhao017@gmail.com>
2022-03-08 17:51:31 +1000

    start boring stuff

--- boringStuff/chapter05/ticTacToe.py
+++ boringStuff/chapter05/ticTacToe.py
@@ -9,4 +9,14 @@ def printBoard(board):
     print('-+-+-')
     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
+turn = 'X'
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
 printBoard(theBoard) 
