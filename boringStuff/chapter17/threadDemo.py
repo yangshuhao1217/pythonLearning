d9e80a2bd8c7055f07af63067e7d74d93dbee1f2
yangshuhao1217 <yangshuhao017@gmail.com>
2022-07-31 23:14:29 +1000

    Working with multithreads

---
+++ boringStuff/chapter17/threadDemo.py
@@ -0,0 +1,12 @@
+from concurrent.futures import thread
+import threading, time
+print('Start of prgram.')
+
+def takeANap():
+    time.sleep(5)
+    print('Wake up!')
+
+threadObj = threading.Thread(target=takeANap)
+threadObj.start()
+
+print('End of program.')
